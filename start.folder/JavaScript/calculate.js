f88bca4451125b8faf67756b4cbeb87bbd21b7dd
takahashi <haya.baseball1114@gmail.com>
2025-04-30 23:01:36 +0900

    work1_5課題

---
+++ start.folder/JavaScript/calculate.js
@@ -0,0 +1,50 @@
+const val1input = document.getElementById("value1");
+const val2input = document.getElementById("value2");
+const operatorselect = document.getElementById("choice");
+const formularea = document.getElementById("formul");
+const resultarea = document.getElementById("result");
+
+val1input.addEventListener("input", calculate);
+val2input.addEventListener("input", calculate);
+operatorselect.addEventListener("change", calculate);
+
+function calculate(){
+    const val1 = parseFloat(val1input.value);
+    const val2 = parseFloat(val2input.value);
+    const operator = operatorselect.value;
+
+    if(isNaN(val1) || isNaN(val2)){
+        formularea.textContent = "計算式";
+        resultarea.textContent = "両方の数値を入力してください。";
+        return;
+    }
+
+    let result;
+    let symbol;  //演算子の記号を表示用に変換
+
+    if(operator === "addition"){
+        result = val1 + val2;
+        symbol = "+";
+    }else if(operator === "subtraction"){
+        result = val1 -val2;
+        symbol = "-";
+    }else if(operator === "multiplication"){
+        result= val * val2;
+        symbol ="*";
+    }else if(operator === "dicision"){
+        if(val2 === 0){
+            formularea.textContent = "計算式";
+            resultarea.textContent = "0で割ることはできません。";
+            return;
+        }
+        result = val1 / val2;
+        symbol = "÷";
+    }else{
+        formularea.textContent = "計算式";
+        resultarea.textContent = "演算子が正しくありません。"
+        return;
+    }
+
+    formularea.textContent = `計算式：${val1} ${symbol} ${val2}`;
+    resultarea.textContent = `計算結果：${result}`;
+}
